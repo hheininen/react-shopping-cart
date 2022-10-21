e44d05c7534106ab68a97a7f31bd3fc7a10305f3
Henrik Heininen <hekuh@hotmail.com>
2022-10-21 20:08:30 +0300

    first commit

---
+++ src/pages/Cancel.js
@@ -0,0 +1,5 @@
+function Cancel() {
+  return <h1>Sorry to see you cancel</h1>;
+}
+
+export default Cancel;
---
+++ src/productsStore.js
@@ -0,0 +1,29 @@
+const productsArray = [
+  {
+    id: "1",
+    title: "Coffee",
+    price: 4.99,
+  },
+  {
+    id: "2",
+    title: "Sunglasses",
+    price: 9.99,
+  },
+  {
+    id: "3",
+    title: "Camera",
+    price: 39.99,
+  },
+];
+
+function getProductData(id) {
+  let productData = productsArray.find((product) => product.id === id);
+  if (productData == undefined) {
+    console.log("ei löydy id:" + id);
+    return undefined;
+  }
+
+  return productData;
+}
+
+export { productsArray, getProductData }; // näitä voi kutsua missä vain appia
